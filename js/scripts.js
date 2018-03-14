498bc7d39b15eee23d91431e5839170574ce2417
Wojtek Gumulski <wojciech.gumulski@gmail.com>
2018-03-14 13:03:19 +0100

    Change in js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,26 @@
+
+
+
 function drawTree(h) {
  	for ( var i = 0 ; i < h ; i++) {
-		var star = '';
-		for (var j = 0 ; j <= i ; j++) {
-			var star = star += '*';
+ 		var star = '*';
+ 		var y = ' ';
+		
+		for (var j = 0 ; j < i ; j++) {
+			var star = star += '**';
 		}
-			console.log(star);
+		
+		for (var k = h ; k > i ; k--) {
+			var y = y + ' ';
+		}
+			
+	console.log(y + star);
+
 	}
+
 }
 
 var a = prompt('podaj wysokość choinki');
+
 drawTree(a); 
+
